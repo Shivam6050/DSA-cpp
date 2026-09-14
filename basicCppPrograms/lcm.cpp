e58e553a74f0557c244cab66568f8613162fa5df
Shivam6050 <60shivam50@gmail.com>
2026-09-14 17:05:27 +0530

    Add implementation for calculating the LCM of two numbers

---
+++ basicCppPrograms/lcm.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int gcd(int a, int b){
+    if(b == 0){
+        return a;
+    }
+    return gcd(b,a%b);
+}
+
+int lcm(int a, int b){
+    return abs(a * b)/gcd(a,b);
+}
+
+int main(){
+    int a,b;
+    cout<<"Enter the first number: ";
+    cin>>a;
+    cout<<"Enter the second number: ";
+    cin>>b;
+    int ans = lcm(a,b);
+    cout<<"The LCM of "<<a<<" and "<<b<<" are: "<<ans;
+}
