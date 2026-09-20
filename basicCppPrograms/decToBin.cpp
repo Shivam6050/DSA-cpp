9fad788adc271a1e9b59a883b30d9cec46a8c310
Shivam6050 <60shivam50@gmail.com>
2026-09-20 10:26:17 +0530

    Add implementation for decimal to binary conversion

---
+++ basicCppPrograms/decToBin.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n = 28;
+    int binary[32] = {0};
+    int i = 0;
+    while(n){
+        binary[i] = n % 2;
+        i++;
+        n /= 2;
+    }
+
+    for(int ind = i - 1; ind >= 0; ind--){
+        cout<<binary[ind];
+    }
+    cout<<endl;
+    return 0;
+}
